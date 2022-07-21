0bfeb42705f4f5ca7c2a4b8f85c5e8968b9d8915
bataevoff <85867199+bataevoff@users.noreply.github.com>
2022-07-21 05:31:55 +0300

    added a test search feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
 import { useEffect, useState } from 'react'
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([])
   const [isLoading, setLoading] = useState(true)
   const [categoryId, setCategoryId] = useState(0)
@@ -31,6 +31,15 @@ const Home = () => {
     window.scrollTo(0, 0)
   }, [categoryId, sortType])
 
+  const pizzas = items.filter(obj => {
+    if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+      return true
+    }
+
+    return false
+  }).map((obj) => (<PizzaBlock key={obj.id} {...obj}/>))
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+
   return (
     <div className='container'>
       <div className="content__top">
@@ -40,8 +49,8 @@ const Home = () => {
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => (<PizzaBlock key={obj.id} {...obj}/>))}
+          ? skeletons
+          : pizzas}
       </div>
     </div>
   );
