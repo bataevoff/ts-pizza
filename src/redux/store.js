9fbf40c3589f9be15a955b4c24d8c803bff65e33
bataevoff <85867199+bataevoff@users.noreply.github.com>
2022-07-29 01:31:15 +0300

    cart slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filter from './filter/filterSlice'
+import cart from './cart/cartSlice'
 
 export const store = configureStore({
   reducer: {
-    filter
+    filter,
+    cart
   },
 })
